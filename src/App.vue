4e3bc681e86e9ec882f1f2de7552ec10950d965d
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2021-11-01 17:31:57 +0300

    добавление роутера

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
   <v-navigation-drawer app v-model="drawer">
       <v-list-item>
+        
         <v-list-item-content>
           <v-list-item-title class="title">
             КИПУ
@@ -19,6 +20,8 @@
           <v-list-item 
           v-for="link in links" 
           :key="link.title"
+          :to="link.url" 
+
           >
             <v-list-item-icon>
               <v-icon>{{ link.icon }}</v-icon>
@@ -36,7 +39,8 @@
     <v-toolbar-items class="hidden-sm-and-down">
        <v-btn 
        v-for="link in links" 
-       :key="link.title" 
+       :key="link.title"
+       :to="link.url"  
        text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn> 
     </v-toolbar-items>
   </v-app-bar>
