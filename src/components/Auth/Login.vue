ce8d6500b143144b23c782313172f0f5dbca0a35
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2021-11-16 22:39:20 +0300

    Создание компонента логин

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -4,21 +4,22 @@
 			<v-flex xs12 sm8 md8>
 			<v-card class="elevetion-12">
 				<v-toolbar dark color="primary">
-Login
-</v-toolbar>
+                    Login
+                </v-toolbar>
 				<v-card-text>
 					<v-form>
 					<v-text-field
-						prepend-icon="mdi-account" 							name="email" 
+					    prepend-icon="mdi-account" 							
+						name="email" 
                			label="Email" 
-type="email">
+                        type="email">
 					</v-text-field>
 					<v-text-field
-prepend-icon="mdi-lock" 
-name="password" 
-label="Password" 
-type="password">
-</v-text-field>
+                       prepend-icon="mdi-lock" 
+                        name="password" 
+                        label="Password" 
+                        type="password">
+                    </v-text-field>
 					</v-form> 
 				</v-card-text>
 				<v-card-actions>
