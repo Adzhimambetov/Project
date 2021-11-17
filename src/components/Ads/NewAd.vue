175ccf235b4b9d5ee5eeba9e6379a35eb4b7e09d
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2021-11-17 14:33:06 +0300

    Объявление переменных и метод

--- src/components/Ads/NewAd.vue
+++ src/components/Ads/NewAd.vue
@@ -63,7 +63,24 @@
 export default {
 	data () { 
 		return {
+		valid: false,
+        title: "",
+        description: "",
+		promo: true
+
 		} 	
+	},
+	methods: {
+      createAd(){
+		if (this.$refs.form.validate()){
+	const ad = {
+		title: this.title,
+		desc: this.description,
+		promo: this.promo
 	}
+	console.log(ad)
+	}
+      }
+    }
 } 
 </script>
