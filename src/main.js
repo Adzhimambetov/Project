c23e915301521b5f2562758cef021225c8f3e4d4
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2021-12-19 23:05:06 +0300

    Подключение Firebase

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from '../router/index'
 import store from './store'
+import fb from 'firebase'
 
 Vue.use(Router)
 Vue.config.productionTip = false
@@ -12,5 +13,19 @@ new Vue({
   vuetify,
   render: h => h(App),
   router:router,
-  store
+  store,
+  created () {
+    const firebaseConfig = {
+      apiKey: "AIzaSyB6-qMUvEv-uWgeAccPTQHsdZZEbjL_3c0",
+      authDomain: "first-project-c374d.firebaseapp.com",
+      projectId: "first-project-c374d",
+      storageBucket: "first-project-c374d.appspot.com",
+      messagingSenderId: "458367420501",
+      appId: "1:458367420501:web:b452424620c84129b4ecdc",
+      measurementId: "G-N0G92B3HNK"
+    };
+    // Initialize Firebase
+  	fb.initializeApp(firebaseConfig);
+  	fb.analytics();
+  },
 }).$mount('#app')
