5d59df1c000ba6b17693c8873a2995182e0fe114
Adzhimambetov Arsen <adzhimambetov.a.i20@gmail.com>
2022-01-24 22:05:10 +0300

    ЛОГИН

--- src/components/Auth/Login.vue
+++ src/components/Auth/Login.vue
@@ -1,73 +1,90 @@
-<template> 
-	<v-container fluid fill-height>
-		<v-layout align-center justify-center> 
-			<v-flex xs12 sm8 md8>
-			<v-card class="elevetion-12">
-				<v-toolbar dark color="primary">
-                  Login
-                </v-toolbar>
-				<v-card-text>
-					<v-form v-model="valid" ref="form"
-					lazy-validation>
-					<v-text-field
-						prepend-icon="mdi-account" 							
-						name="email" 
-                        type="email"
-						v-model="email"
-						:rules="emailRules" >
-					</v-text-field>
-					<v-text-field
-                      prepend-icon="mdi-lock" 
-                      name="password" 
-                      label="Password" 
-                      type="password"
-					v-model="password"
-					:rules="passwordRules">
-                    </v-text-field>
-					</v-form> 
-				</v-card-text>
-				<v-card-actions>
-					<v-spacer></v-spacer>
-					<v-btn color="primary"
-					@click="onSubmit"
-					:disabled="!valid">Login</v-btn>
-				</v-card-actions>	
-			</v-card>
-			</v-flex>
-		</v-layout> 
-	</v-container>
-</template>
+	<template> 
+		<v-container fluid fill-height>
+			<v-layout align-center justify-center> 
+				<v-flex xs12 sm8 md8>
+				<v-card class="elevetion-12">
+					<v-toolbar dark color="primary">
+					Login
+					</v-toolbar>
+					<v-card-text>
+						<v-form v-model="valid" ref="form"
+						lazy-validation>
+						<v-text-field
+							prepend-icon="mdi-account" 							
+							name="email" 
+							type="email"
+							v-model="email"
+							:rules="emailRules" >
+						</v-text-field>
+						<v-text-field
+						prepend-icon="mdi-lock" 
+						name="password" 
+						label="Password" 
+						type="password"
+						v-model="password"
+						:rules="passwordRules">
+						</v-text-field>
+						</v-form> 
+					</v-card-text>
+					<v-card-actions>
+						<v-spacer></v-spacer>
+						<v-btn 
+						color="primary"
+						@click="onSubmit"
+						:loading="loading"
+						:disabled="!valid || loading">
+							Login
+						</v-btn>
 
-<script>
-export default {
-	data () { 
-		return {
-			email: "",
-	password: "",
-	valid: false,
-	emailRules: [
-				v => !!v || 'E-mail is required',
-			v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
-			],
-			passwordRules: [
-				v => !!v || 'Name is required',
-			v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
-			]
+					</v-card-actions>	
+				</v-card>
+				</v-flex>
+			</v-layout> 
+		</v-container>
+	</template>
+
+	<script>
+	export default {
+		data () { 
+			return {
+				email: "",
+		password: "",
+		valid: false,
+		emailRules: [
+					v => !!v || 'E-mail is required',
+				v => /.+@.+\..+/.test(v) || 'E-mail must be valid'
+				],
+				passwordRules: [
+					v => !!v || 'Name is required',
+				v => (v && v.length >= 6) || 'Password must be more or equel than 6 characters',
+				]
 
 
-		} 	
-	},
-	methods: {
-		onSubmit(){
-			if (this.$refs.form.validate()){
-				const user = {
-					email: this.email,
-					password: this.password
-				}
-				console.log(user)
+			} 	
+		},
+		methods: {
+				onSubmit(){
+		if (this.$refs.form.validate()){
+			const user = {
+				email: this.email,
+				password: this.password
 			}
+			this.$store.dispatch('loginUser', user)
+			.then(() => {
+				this.$router.push("/")
+			})
+			.catch((err) => {
+				console.log(err.message)
+			})
 		}
 	}
+		},
+		computed: {
+	loading() {
+		return this.$store.getters.loading
+	}
+}
 
-} 
-</script>
+
+		} 
+	</script>
--- src/store/user.js
+++ src/store/user.js
@@ -1,32 +1,54 @@
-import fb from 'firebase'
+    import fb from 'firebase'
 
-class User {
-	constructor(id) {
-		this.id = id
-	}
-}
+    class User {
+        constructor(id) {
+            this.id = id
+        }
+    }
 
-export default {
-	state: {
-     user: null
-},
-	mutations: {
-        setUser(state,payload) {
-            state.user = payload }   
+    export default {
+        state: {
+        user: null
     },
-	actions: {
-        registerUser({commit},{email, password}){
-            fb.auth().createUserWithEmailAndPassword(email,password).then(response => {
-                  console.log(response.user.uid)
-                  commit('setUser', new User(response.user.uid))
-  })
-  }
-  
-    },
-	getters: {
-     user(state) {
-            return state.user
-	}
-}
-}
+        mutations: {
+            setUser(state,payload) {
+                state.user = payload }   
+        },
+        actions: {
+            async registerUser({commit},{email, password}){
+                commit('clearError')
+                commit('setLoading', true)
+                try {
+            const response = await 
+            fb.auth().createUserWithEmailAndPassword(email,password)
+                    commit('setUser', new User(response.user.uid))
+                    commit('setLoading', false)
+                } catch (error) {
+                    commit('setLoading', false)
+                    commit('setError', error.message)
+            throw error
+                }
+            },
+            async loginUser({commit},{email, password}){
+                commit('clearError')
+                commit('setLoading', true)
+                try {
+                const response = await 
+                fb.auth().signInWithEmailAndPassword(email,password)
+                    commit('setUser', new User(response.user.uid))
+                    commit('setLoading', false)
+                    } catch (error) {
+                    commit('setLoading', false)
+                    commit('setError', error.message)
+            throw error
+                }
+    }
+    
+        },
+        getters: {
+        user(state) {
+                return state.user
+        }
+    }
+    }
 
